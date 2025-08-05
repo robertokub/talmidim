c766b3e3f27da0fa72490cc26fb62b05872425ce
ROBERTO LIMA <roberto.kubb@gmail.com>
2025-08-05 13:59:13 -0300

    index.tsx

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { collection, getDocs } from "firebase/firestore"; // <-- Funções para ler dados
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
